6ce4509a194645d8624e321b98ccf5664c21426d
DKJKOH <kohDamien1998@gmail.com>
2023-08-03 17:34:14 +0800

    Modified player controls and Player camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Reference to main camera
+    Camera cam;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  Get main camera
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        // forward and backward movement
+        float forward = Input.GetAxisRaw("Vertical");
+        // left and right movement
+        float sideways = Input.GetAxisRaw("Horizontal");
+
+        // Update the player's translation over time
+        Vector3 newPos = new Vector3(sideways, 0, forward) * movementSpeed * Time.deltaTime;
+
+        // Add the new position with the current position of the object the script is attached to
+        rb.MovePosition(transform.position + newPos);
+
+        // Prevents bouncing player stops drifting
+        if (forward == 0 && sideways == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/cameraControls.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControls : MonoBehaviour
+{
+    // Target object camera will look at
+    public Transform target;
+
+    // How fast camera moves 
+    public float cameraSpeed;
+
+    // Camera offset from target value
+    public Vector2 offSetValue;
+
+    private Vector3 offset;
+
+    // Distacne between target and camera
+    private double distance;
+
+    // How far you want the target to go before camera starts to pan to target
+    [SerializeField] double edgeDistance;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Maintain distance from scene during tracking
+        offset = (Vector3)offSetValue;
+        offset.y = transform.position.y - target.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate distance between  camera and target
+        distance = Vector3.Distance(transform.position, target.position);
+    }
+    void LateUpdate()
+    {
+        // If the target is too far away from camera
+        if (distance > edgeDistance)
+        {
+            // Camera moves towards target
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target.position + offset,
+                cameraSpeed * Time.deltaTime
+            );
+        }
+
+
+    }
+}
