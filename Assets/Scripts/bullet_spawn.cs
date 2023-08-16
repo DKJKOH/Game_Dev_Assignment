2a6014b8d11b152381fd38d337d00688c952d4be
Fabian Tan <102579078+fabianTYZ@users.noreply.github.com>
2023-08-16 12:12:54 +0800

    bullet movement, spawn and despawn

---
+++ Assets/Scripts/bullet_spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_spawn : MonoBehaviour
+{
+    public GameObject bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//upon left click
+        if(Input.GetButtonDown("Fire1"))
+        {
+        	//Create bullet object
+        	Instantiate(bullet, transform.position, transform.rotation);
+        }
+
+
+        //upon hitting enemy/wall
+
+
+        // Bullet object Disappear
+
+    }
+}
