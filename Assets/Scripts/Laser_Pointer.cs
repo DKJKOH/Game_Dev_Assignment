ecd2e1e53dc95f818ab5436348e3cc1857145dbe
DKJKOH <kohDamien1998@gmail.com>
2023-08-18 08:51:32 +0800

    Fixed laser directional issues, Fixed player moving by itself without player input

--- Assets/Scripts/Laser_Pointer.cs
+++ Assets/Scripts/Laser_Pointer.cs
@@ -11,7 +11,11 @@ public class Laser_Pointer : MonoBehaviour
 
     // Set your laser distance here
     [SerializeField]
-    float laserDist = 10;
+    int laserDist = 10;
+
+    // Set camera which follows user here
+    [SerializeField]
+    Camera mainCamera;
 
     // Renders Line
     private LineRenderer lineRenderer;
@@ -32,31 +36,72 @@ public class Laser_Pointer : MonoBehaviour
 
         // Get laser's position and the forward direction (y direction in our case)
         Vector2 laserObjectPos = laserObject.position;
-        Vector2 laserObjectDir = laserObject.up;
+
+        //Vector2 laserObjectDir = laserObject.up;
+
+        // Retrieve mouse position (pixel coordinates)
+        Vector2 mousePosition = Input.mousePosition;
+        // Convert pixel coordinates into world coordinates
+        Vector3 mousePositionWorldCoordinates = mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, mainCamera.transform.position.y - laserObject.position.y));
+
+        Vector3 laserDirection = mousePositionWorldCoordinates - laserObject.position;
+
+
 
         // Create raycast from laser object in the forward direction of the laser object
-        RaycastHit2D rayHit = Physics2D.Raycast(laserObjectPos, laserObjectDir, laserDist);
+        //RaycastHit2D rayHit = Physics2D.Raycast(laserObjectPos, laserObjectDir, laserDist);
 
-        // Update the Line Renderer's end position to the hit point
-        lineRenderer.SetPosition(0, laserObjectPos);
 
-        // This if statement is to bypass error "object reference not set to an instance of an object"
-        if (rayHit.collider == null)
+        // Debug show ray
+        //Debug.DrawRay(laserObjectPos, laserObjectDir);
+
+        // Stores details on what the ray hit
+        RaycastHit rayHit;
+
+        // Debug, shows ray
+        Debug.DrawRay(laserObject.position, laserDirection);
+
+        // Set starting position of laser (which is at weapon)
+        lineRenderer.SetPosition(0, laserObject.position);
+
+        // If ray hits something
+        if (Physics.Raycast(laserObject.position, laserDirection, out rayHit, laserDist))
         {
-            // Default to laserDist value from object
-            lineRenderer.SetPosition(1, laserObjectPos + laserObjectDir * laserDist);
-        }
-        // If raycast hits wall
-        else if (rayHit.collider.tag == "Wall" || rayHit.collider.tag == "Person")
-        {
-            // Update the Line Renderer's end position to the hit point
+            Debug.Log("Apple");
+            // Set the laser end point
             lineRenderer.SetPosition(1, rayHit.point);
         }
         else
         {
-
-            // Default to laserDist value from object
-            lineRenderer.SetPosition(1, laserObjectPos + laserObjectDir * laserDist);
+            lineRenderer.SetPosition(1, laserObject.position + laserDirection.normalized * laserDist);
         }
+
+        // Issue, you need to provide the direction of the ray as the second parameter, not the end point
+
+        //    if (rayHit.collider != null)
+        //    {
+        //        // Update the Line Renderer's end position to the hit point
+        //        lineRenderer.SetPosition(0, laserObjectPos);
+
+        //        // If raycast hits wall or person
+        //        if (rayHit.collider.tag == "Wall" || rayHit.collider.tag == "Person")
+        //        {
+        //            // Update the Line Renderer's end position to the hit point
+        //            lineRenderer.SetPosition(1, rayHit.point);
+        //        }
+        //        else
+        //        {
+        //            // Update the Line Renderer's end position to the hit point
+        //            lineRenderer.SetPosition(1, rayHit.point);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // Update the Line Renderer's start position at the gun's coordinates
+        //        lineRenderer.SetPosition(0, laserObjectPos);
+
+        //        // Default to laserDist value from object
+        //        lineRenderer.SetPosition(1, laserObjectPos + laserObjectDir * laserDist);
+        //    }
     }
 }
