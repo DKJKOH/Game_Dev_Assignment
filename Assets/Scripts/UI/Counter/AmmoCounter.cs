42bb6ed19a1fedcf32c997fb7370f49d41bbd4d4
NomStop <43531339+NomStop@users.noreply.github.com>
2023-08-23 21:59:28 +0800

    In Game UI Counter

---
+++ Assets/Scripts/UI/Counter/AmmoCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCounter : MonoBehaviour
+{
+    public int ammo;
+    public Text ammoDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoDisplay.text = "Ammo Left: " + ammo.ToString();
+    }
+
+    public int getAmmo()
+    {
+        return ammo;
+    }
+
+    public void setAmmo(int NewAmmo)
+    {
+        ammo = NewAmmo;
+    }
+}
---
+++ Assets/Scripts/UI/Counter/EnemyCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCounter : MonoBehaviour
+{
+    public int enemy;
+    public Text enemyDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyDisplay.text = "Enemies Left: " + enemy.ToString();
+    }
+
+     public int getEnemy()
+    {
+        return enemy;
+    }
+
+    public void setEnemy(int NewEnemy)
+    {
+        enemy = NewEnemy;
+    }
+}
