2a807ab957a3ff66f21d38ffe728c76cedc684be
NomStop <43531339+NomStop@users.noreply.github.com>
2023-08-23 16:43:33 +0800

    Main and Game Over Menu

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    
+
+    // Restart Button
+    public void RestartButton() {
+        //Reload the same level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Return Main Menu Button
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+        
+    
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Play Game Button
+    public void PlayGame()
+    {
+        // To load the next active stage
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    //Quit Game Button
+    public void QuitGame() 
+    {
+        Debug.Log("Quit!"); //To test if the quit button works
+        Application.Quit();
+    }
+}
