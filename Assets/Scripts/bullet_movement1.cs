2a0afe1640f1f0172f0fad11a57b0993301b05b1
unknown <alstonleejc@gmail.com>
2023-08-29 17:37:24 +0800

    Added palettes for map additions

---
+++ Assets/Scripts/bullet_movement1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_movement : MonoBehaviour
+{
+
+	[SerializeField]
+	float force = 1000;
+
+	Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// calling rigidbody2d
+        rb = GetComponent<Rigidbody2D>();
+
+        // Adding bullet force (In Y direction as it is 2D)
+        rb.AddForce(transform.up * force * Time.deltaTime, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	
+    }
+}
