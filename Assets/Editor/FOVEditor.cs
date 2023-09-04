e7ce26ea4cb9b48b3dbdc1c147006059a36f4f66
DKJKOH <kohDamien1998@gmail.com>
2023-09-05 01:31:48 +0800

    Implemented detection of targets using raycasts, obstacles will block the raycast from targets

---
+++ Assets/Editor/FOVEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(FOV))]
+public class FOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FOV fieldOfVision = (FOV)target;
+
+        // Draw the radius of fov
+        Handles.color = Color.yellow;
+
+        // Draws circle
+        Handles.DrawWireArc(fieldOfVision.transform.position, Vector3.forward, Vector3.up, 360, fieldOfVision.viewRadius);
+
+        // Player vision cone lines
+        Vector3 viewCone1 = fieldOfVision.DirectionFromAngle(-fieldOfVision.viewAngle / 2, false);
+        Vector3 viewCone2 = fieldOfVision.DirectionFromAngle(fieldOfVision.viewAngle / 2, false);
+        Handles.DrawLine(fieldOfVision.transform.position, fieldOfVision.transform.position + viewCone1 * fieldOfVision.viewRadius);
+        Handles.DrawLine(fieldOfVision.transform.position, fieldOfVision.transform.position + viewCone2 * fieldOfVision.viewRadius);
+
+        // Change color line
+        Handles.color = Color.red;
+        // Iterate through the current targets seen by the object
+        foreach (Transform seen_target in fieldOfVision.targetsVisible)
+        {
+            // Draw line from object to target
+            Handles.DrawLine(fieldOfVision.transform.position, seen_target.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Vision/FOV.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    // View radius of player
+    public float viewRadius;
+
+    // Player's vision cone angle
+    [Range(0,360)]
+    public float viewAngle;
+
+    // Enemy and obstacle masks
+    public LayerMask targetMask;
+    public LayerMask ObstacleMask;
+
+    // Lists which contains current visible targets from current object
+    [HideInInspector]
+    public List<Transform> targetsVisible = new List<Transform>();
+
+    private void Start()
+    {
+        // Starts the find targets with delay
+        StartCoroutine("FindTargets_Delay", .2f);
+    }
+    /* Function which finds targets (with delay) */
+    IEnumerator FindTargets_Delay(float delay)
+    {
+        while(true)
+        {
+            // Wait for few seconds before continuing with code after this line
+            yield return new WaitForSeconds(delay);
+
+            findVisibleTargets();
+        }
+    }
+
+
+    /* Function which finds targets in current object's view cone */
+    void findVisibleTargets()
+    {
+        // Clears the list of visible targets
+        targetsVisible.Clear();
+
+        // Basically detects if the enemy is within the player CIRCLE (Not the cone)
+        //Collider[] targetsWithinVision = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        // A 2D circle checking if there circle overlaps with any rigidbody2D
+        Collider2D[] targetsWithinVision = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsWithinVision.Length; ++i)
+        {
+            // Retrieve target details
+            Transform target = targetsWithinVision[i].transform;
+
+            // Retrieve direction to target
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            //Debug.Log(Vector3.Angle(transform.up, directionToTarget));
+
+            // If target is within the view angle
+            if (Vector3.Angle(transform.up, directionToTarget) < viewAngle / 2)
+            {
+                // Retrieve distance from current object to target
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                //// Raycast to check if there are obstacles from player to target
+                //if (!Physics.Raycast(new Vector3(transform.position.x, transform.position.y, 0), directionToTarget, distanceToTarget, ObstacleMask))
+                //{
+                //    // Add the current target into the list
+                //    targetsVisible.Add(target);
+
+                //    // If there are no obstacles to target
+
+                //    // Insert code here
+                //}
+
+                // Raycast to check if there are obstacles from player to target
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, ObstacleMask))
+                {
+                    // Add the current target into the list
+                    targetsVisible.Add(target);
+
+                    // If there are no obstacles to target
+
+                    // Insert code here
+                }
+            }
+        }
+    }
+
+
+    public Vector3 DirectionFromAngle(float angleDegrees, bool isAngleGlobal)
+    {
+        // Convert angles to local angles instead
+        if (!isAngleGlobal)
+        {
+            // This portion rotates the arc lines towards the mouse position
+            angleDegrees -= transform.eulerAngles.z;
+        }
+
+        return new Vector3(Mathf.Sin(angleDegrees * Mathf.Deg2Rad), Mathf.Cos(angleDegrees * Mathf.Deg2Rad),0);
+    }
+}
