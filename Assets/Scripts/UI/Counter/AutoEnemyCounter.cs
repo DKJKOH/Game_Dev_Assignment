ca55aab9fb26fa53f214180cf5d2cf872d281b15
NomStop <43531339+NomStop@users.noreply.github.com>
2023-09-08 15:43:36 +0800

    Update on Counter
    Changing the in game counter scripts to update automatically rather than manually.

---
+++ Assets/Scripts/UI/Counter/AutoEnemyCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoEnemyCounter : MonoBehaviour
+{
+    GameObject[] enemies;
+    public Text enemyCountText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // To Find How Many Enemies are in the Level
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        // To Display Enemies Left
+        enemyCountText.text = "Enemies Left : " + enemies.Length.ToString();
+    }
+}
---
+++ Assets/Scripts/UI/Counter/AutoHostageCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoHostageCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject[] hostages;
+    public Text hostageCountText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // To Find How Many Hostages are in the Level
+        hostages = GameObject.FindGameObjectsWithTag("Hostage");
+        // To Display Hostages Left
+        hostageCountText.text = "Hostages Left : " + hostages.Length.ToString();
+    }
+}
